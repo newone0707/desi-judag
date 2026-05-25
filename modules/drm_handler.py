195845af0c073d869be3bc3ff16ccd6ffd840aae
botdlbot9-rgb <botdlbot9@gmail.com>
2026-05-25 15:47:49 +0530

    drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -370,7 +370,7 @@ async def drm_handler(bot: Client, m: Message):
                     keys_string = ""
 
             elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
-                url = f"https://anonymouspwplayerr-3cfbfedeb317.herokuapp.com/pw?url={url}&token={pwtoken}"
+                url = f"https://anonymouspwplayer-ce3f42358cca.herokuapp.com/pw?url={url}&token={pwtoken}"
                 keys_string = ""
                                       
             elif 'encrypted.m' in url:
